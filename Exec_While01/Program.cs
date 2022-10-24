b0f492172ba6bcbc087b250bd9513b8249a0ad01
88693 <88693@LAPTOP-FS9ST1LD>
2022-10-24 23:09:41 +0800

    加入專案檔案。

---
+++ Exec_While01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_While01
+{//Q1 1+ 2 + 3 + 4 + .... + N<105請問 N 是多少?
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int total = 0;
+			int N = 1;
+
+			while(total < 105)
+			{
+				if (total + N >= 105) break;
+				total += N;
+				N += 1;
+			}
+
+			Console.WriteLine(N-1);
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{//Q2使用 while , 在畫面顯示所有可以整除 300 的整數,例如: 1, 2, 3, 4, 5, 6, 10, ...., 300
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int number1 = 300;
+			int result = 1;
+
+			while (result < number1)
+			{
+				if (number1 % result == 0)
+				{
+					Console.WriteLine(result);
+				}
+				result += 1;
+			}
+		}
+	}
+}
